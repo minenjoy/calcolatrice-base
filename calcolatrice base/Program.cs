1f088d6e96f2d8289cd839704ed6ccdfb2bbdd27
Gabriele Gostinfini <gostygabri@gmail.com>
2023-12-11 23:05:13 +0100

    Versione completa (almeno per ora)

--- calcolatrice base/Program.cs
+++ calcolatrice base/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics;
 
 namespace calcolatrice_base
 {
@@ -6,87 +7,267 @@ namespace calcolatrice_base
     {
         static void Main(string[] args)
         {
-            string type;
-            double num1;
-             double num2;
-            double risult ;
-            bool othernum = true;
-            Console.WriteLine("Che tipo di operazione vuoi effettuare");
-            Console.WriteLine("digita + - * / radice potenza");
-            type = Console.ReadLine();
+            string type="";
+            int check = 0;
+            double risult = 0 ;
+            do
+            {
+                if (check!=0)
+                {
+                    Console.WriteLine("formato errato, selezionare un formato corretto");
+                }
+                Console.WriteLine("Che tipo di operazione vuoi effettuare");
+                Console.WriteLine("digita + - * / radice potenza");
+                        type = Console.ReadLine();
+                        type= type.ToLower();
+                check = check+1;
+
+            } while (type==""||(type!="+" && type != "-" && type != "*" && type != "/" && type != "potenza" && type != "radice"));
+
             switch (type)
             {
                 case "+":
                     Console.WriteLine("hai selezionato addizione");
-                    
-                    Console.WriteLine("scrivi il primo numero");
-                    num1 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("scrivi il secondo numero");
-                    num2 = double.Parse(Console.ReadLine());
-
-                    risult = num1 + num2;
-                    Console.WriteLine("il risultato è " + risult); 
+                    risult = Somma();
                     break;
                 case "-":
                     Console.WriteLine("hai selezionato sottrazione");
-                    
-                    Console.WriteLine("scrivi il primo numero");
-                    num1 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("scrivi il secondo numero");
-                    num2 = double.Parse(Console.ReadLine());
-
-                    risult = num1 - num2;
-                    Console.WriteLine("il risultato è " + risult); 
+                    risult = Differenza();
                     break;
                 case "*":
                     Console.WriteLine("hai selezionato moltiplicazione");
-                    
-                    Console.WriteLine("scrivi il primo numero");
-                    num1 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("scrivi il secondo numero");
-                    num2 = double.Parse(Console.ReadLine());
-
-                    risult = num1 * num2;
-                    Console.WriteLine("il risultato è " + risult);
+                    risult = prodotto();
                     break;
                 case "/":
                     Console.WriteLine("hai selezionato divisione");
-                    
-                    Console.WriteLine("scrivi il primo numero");
-                    num1 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("scrivi il secondo numero");
-                    num2 = double.Parse(Console.ReadLine());
-
-                    risult = num1 / num2;
-                    Console.WriteLine("il risultato è " + risult);
+                    risult = Diviso();
                     break;
                 case "radice":
                     Console.WriteLine("hai selezionato radice quadrata");
                     
-                    Console.WriteLine("scrivi il primo numero");
-                    num1 = double.Parse(Console.ReadLine());
-
-                    risult = Math.Sqrt(num1);
-                    Console.WriteLine("il risultato è " + risult);
+                    risult = radice();
                     break;
                 case "potenza":
                     Console.WriteLine("hai selezionato elevamento a potenza");
 
-                    Console.WriteLine("scrivi il primo numero");
-                    num1 = double.Parse(Console.ReadLine());
-
-                    Console.WriteLine("scrivi il secondo numero");
-                    num2 = double.Parse(Console.ReadLine());
-
-                    risult = Math.Pow(num1, num2);
-                    Console.WriteLine("il risultato è " + risult);
+                    risult = elevamento();
                     break;
             }
+            Console.WriteLine("il risultato è " + risult);
+        }
+    static double Somma()
+        {
+            double risult;
+            double num1= -500000000000000002.0;
+            double num2=0;
 
+            do
+            {
+
+                Console.WriteLine("scrivi il primo numero");
+                try
+                    {
+                     num1 = double.Parse(Console.ReadLine());
+                    }
+                catch (Exception)
+                    {
+                    Console.WriteLine("c'è stato un errore, riprova");
+                
+                    }
+            } while (num1<(-50000000000000000.0));
+            do
+            {
+                Console.WriteLine("scrivi il secondo numero"); 
+                try
+                    {
+                     num2 = double.Parse(Console.ReadLine());
+                    }
+                catch (Exception)
+                {
+                Console.WriteLine("c'è stato un errore, riprova");
+                   
+            }
+            } while (num2 < -50000000000000000);
+            risult = num1 + num2;
+            Console.WriteLine(num1);
+                return risult;
+
+        }
+
+    static double Differenza()
+        {
+            double risult;
+            double num1 = -500000000000000002.0;
+            double num2 = 0;
+
+            do
+            {
+
+                Console.WriteLine("scrivi il primo numero");
+                try
+                {
+                    num1 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num1 < (-50000000000000000.0));
+            do
+            {
+                Console.WriteLine("scrivi il secondo numero");
+                try
+                {
+                    num2 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num2 < -50000000000000000);
+            risult = num1 - num2;
+            Console.WriteLine(num1);
+            return risult;
+
+        }
+
+        static double prodotto()
+        {
+            double risult;
+            double num1 = -500000000000000002.0;
+            double num2 = 0;
+
+            do
+            {
+
+                Console.WriteLine("scrivi il primo numero");
+                try
+                {
+                    num1 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num1 < (-50000000000000000.0));
+            do
+            {
+                Console.WriteLine("scrivi il secondo numero");
+                try
+                {
+                    num2 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num2 < -50000000000000000);
+            risult = num1 * num2;
+            Console.WriteLine(num1);
+            return risult;
+
+        }
+
+        static double Diviso()
+        {
+            double risult;
+            double num1 = -500000000000000002.0;
+            double num2 = 0;
+
+            do
+            {
+
+                Console.WriteLine("scrivi il primo numero");
+                try
+                {
+                    num1 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num1 < (-50000000000000000.0));
+            do
+            {
+                Console.WriteLine("scrivi il secondo numero");
+                try
+                {
+                    num2 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num2 < -50000000000000000);
+            risult = num1 / num2;
+            Console.WriteLine(num1);
+            return risult;
+
+        }
+
+        static double elevamento()
+        {
+            double risult;
+            double num1 = -500000000000000002.0;
+            double num2 = 0;
+
+            do
+            {
+
+                Console.WriteLine("scrivi il primo numero");
+                try
+                {
+                    num1 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num1 < (-50000000000000000.0));
+            do
+            {
+                Console.WriteLine("scrivi il secondo numero");
+                try
+                {
+                    num2 = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("c'è stato un errore, riprova");
+
+                }
+            } while (num2 < -50000000000000000);
+            risult = Math.Pow(num1, num2);
+            Console.WriteLine(num1);
+            return risult;
+
+        }
+
+        static double radice()
+        {
+            double risult;
+            double num1 = 0;
+            do
+            {
+                Console.WriteLine("scrivi il primo numero");
+                try
+                    {
+                        num1 = double.Parse(Console.ReadLine());
+                    }
+                catch (Exception)
+                    {
+                        Console.WriteLine("c'è stato un errore, riprova");
+                    }
+            } while (num1 < -50000000000000000);
+            risult = Math.Sqrt(num1);
+            return risult;
         }
     }
 }
